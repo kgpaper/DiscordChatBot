c6ecacc43d52d0ff0de01b46fb0964a86efd83f1
TaegyeongLee <luckperson7@naver.com>
2018-05-20 20:24:40 +0900

    vote html

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
-
 
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+@application.route("/createvote")
+def create_vote():
+    return render_template('vote.html')
 
 
 if __name__ == "__main__":
